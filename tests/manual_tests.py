c99405abf85777dfd5c5460c0d75c38b47554d68
Tim Marquart <tmarquart@gmail.com>
2025-10-01 17:24:17 -0500

    Added manual tests

--- tests/manual_tests.py
+++ tests/manual_tests.py
@@ -4,11 +4,41 @@ def func_a(a,b,c):
 def func_b(d,e,f):
     return d * e + f
 
+class StandardClass(object):
+    def __init__(self):
+        pass
 
-@sg.combine(func_a,func_b)
+    @sg.fuse(func_a,func_b)
+    def wrapper(*args,**kwargs):
+        pass
+
+class BaseClass(object):
+    def __init__(self):
+        pass
+    def base_func(self,a,b,c):
+        return a+b+c
+
+class ChildClass(BaseClass):
+    @sg.fuse(BaseClass.base_func)
+    def child_func(self,*args,**kwargs):
+        return a*b+c
+
+@sg.fuse(ChildClass.child_func,func_b)
+def top_function(*args,**kwargs):
+    print(a+b+c)
+    print(func_b(func_b.vars.unpack()))
+
+@sg.fuse(func_a)
+def ext_func(new_input=0,*args,**kwargs):
+    print(func_a(func_a.vars.unpack()))
+    print(new_input)
+
+@sg.fuse(func_a,func_b)
 def wrapper(*args,**kwargs):
-    print(func_a(**func_a.vars.unpack()))
-    print(func_b(**func_b.vars.unpack()))
+    print(func_a(func_a.vars.unpack()))
+    print(func_b(func_b.vars.unpack()))
 
 if __name__=='__main__':
     wrapper(1,2,3,4,5,6)
+    cl=StandardClass()
+    cl.wrapper(1,2,3,4,5,6)
